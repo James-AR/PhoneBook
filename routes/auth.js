4ffac7d53756fb6682a034a87d309b2d34f82e20
James <ruiz.james12@yahoo.com>
2019-07-03 11:17:05 -0700

    Login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,19 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const auth = require('../middleware/auth')
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
-router.get('/', (req, res) => {
-    res.send('Getting logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+
+    }   catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
 })
 
 router.post('/', [
