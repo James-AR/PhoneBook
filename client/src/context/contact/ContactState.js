75c09699fd4c0ffde300a79d662b47ae5e61a6dd
James <ruiz.james12@yahoo.com>
2019-07-09 08:29:21 -0700

    Add contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -23,10 +23,18 @@ const ContactState = props => {
     }
 
     const [state, dispatch] = useReducer(ContactReducer, initialState)
+
+    const addContact = contact => {
+        contact.id = uuid.v4()
+        dispatch({ type: ADD_CONTACT, payload: contact })
+    }
     
     return (
         <ContactContext.Provider 
-        value={{ contacts: state.contacts }}>
+        value={{ 
+            contacts: state.contacts,
+            addContact
+            }}>
             {props.children}
         </ContactContext.Provider>
     )
